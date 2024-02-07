2bf17e4f839dc814bb7a39a1ba4b645505480c19
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-07 23:06:00 +0000

    Added the weather component.

--- src/Search.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Search() {
-  return <p>Hello World</p>;
-}
-
-export default Search;
---
+++ src/Weather.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Weather.css";
+
+function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Weather App</h1>
+      <form className="weather-form">
+        <input type="text" placeholder="Enter a city" />
+        <button className="search-button">Search</button>
+      </form>
+      <main className="weather-container">
+        <div className="weather-details">
+          <h2>London</h2>
+          <p>
+            <span>Wednesday 12:25</span> <br />
+            <span>February 6,2024</span>
+          </p>
+          <p>
+            Light Rain
+            <br />
+            Humidity: 92%
+            <br />
+            Wind: 3mph
+          </p>
+        </div>
+        <div className="temperature">
+          <p>
+            <span>🌧️</span> <span>7℃</span>
+          </p>
+        </div>
+      </main>
+      <section className="weather-forecast">
+        <div>
+          <p>Thursday</p>
+          <p>🌧️</p>
+          <p>12℃ | 4℃</p>
+        </div>
+        <div>
+          <p>Friday</p>
+          <p>🌦️</p>
+          <p>12℃ | 8℃</p>
+        </div>
+        <div>
+          <p>Saturday</p>
+          <p>🌧️</p>
+          <p>12℃ | 5℃</p>
+        </div>
+        <div>
+          <p>Sunday</p>
+          <p>🌤️</p>
+          <p>10℃ | 3℃</p>
+        </div>
+        <div>
+          <p>Monday</p>
+          <p>🌦️</p>
+          <p>8℃ | 2℃</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Weather;
